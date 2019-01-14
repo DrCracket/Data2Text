af34ec78bd485fa1101a1e1b0b702054165662dd
Jonas Belouadi <bigjonas96@gmail.com>
2019-01-14 22:36:14 +0100

    Added main method to access all models

---
+++ main.py
@@ -0,0 +1,10 @@
+###############################################################################
+# Control room to acces Information Extraction, Content Selection & Planning  #
+# and Text Generation Module
+###############################################################################
+
+import extractor
+# import planner
+
+extractor.get_extractor()
+# planner.train_planner(extractor)
