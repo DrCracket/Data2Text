653c908b2732c0ed7ec583e334266370eb10aad3
Jonas Belouadi <bigjonas96@gmail.com>
2019-03-12 15:39:18 +0100

    Updated max_len

--- util/constants.py
+++ util/constants.py
@@ -37,8 +37,8 @@ NUM_PLAYERS = 13
 MAX_RECORDS = 2 * NUM_PLAYERS * len(bs_keys) + 2 * len(ls_keys)
 
 # limit the content plan length to forcefully prevent content plans as long as
-# the record table the maximum content plan length in the training set is 124
-MAX_CONTENT_PLAN_LENGTH = 124
+# the record table the maximum content plan length in the training set is 128
+MAX_CONTENT_PLAN_LENGTH = 128
 MIN_CONTENT_PLAN_LENGTH = 30
 
 TEXT_MAX_LENGTH = 1000
